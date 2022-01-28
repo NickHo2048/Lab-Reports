f3efc1097f1b0a3035f43a03d47db4a4f954020e
NickHo2048 <niho@ucsd.edu>
2022-01-28 13:33:37 -0800

    MarkdownParse.java can now handle test2

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -20,9 +20,17 @@ public class MarkdownParse {
         return toReturn;
     }
     public static void main(String[] args) throws IOException {
-		Path fileName = Path.of(args[0]);
-	    String contents = Files.readString(fileName);
+        
+        try{
+        Path fileName = Path.of(args[0]);
+        String contents = Files.readString(fileName);
         ArrayList<String> links = getLinks(contents);
         System.out.println(links);
+        }
+        catch(Exception e){
+            System.out.println(e.toString());
+            return;
+        }
+	    
     }
 }
