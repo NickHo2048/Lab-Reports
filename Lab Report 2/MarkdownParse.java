30c848d7ea7f71a06ec810d95b0c28a3a927a1f2
NickHo2048 <niho@ucsd.edu>
2022-01-28 14:06:00 -0800

    Update MarkdownParse.java

--- Lab Report 2/MarkdownParse.java
+++ Lab Report 2/MarkdownParse.java
@@ -14,10 +14,6 @@ public class MarkdownParse {
             int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
             int openParen = markdown.indexOf("(", nextCloseBracket);
             int closeParen = markdown.indexOf(")", openParen);
-            if(markdown.substring(openParen + 1, closeParen).contains(" ")){
-                continue;
-            }
-            else toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
         }
         return toReturn;
